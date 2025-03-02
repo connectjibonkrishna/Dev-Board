1d1f38021f369d826276eb41cbdfc4e561f99a38
Jibon Krishna <jibonkrishnaroysetu@gmail.com>
2025-03-02 22:15:05 +0600

    added functionality to theme color button

--- color.js
+++ color.js
@@ -4,11 +4,29 @@ const colorPalette = [
   "bg-purple-100",
   "bg-orange-100",
   "bg-teal-100",
-  "bg-green-100",
+  "bg-lime-100",
   "bg-red-100",
+  "bg-violet-100",
+  "bg-fuchsia-100",
+  "bg-rose-100"
 ];
 // console.log(colorPalette);
 // for (color of colorPalette){
 //   console.log(color);
 // }
 
+const themeBtn = document.getElementById("theme-btn");
+const colorToBody = document.getElementById("body");
+
+function changeColor() {
+  let randomIndexNumber = Math.floor(Math.random() * colorPalette.length);
+
+  let randomColor = colorPalette[randomIndexNumber];
+
+  for (const color of colorPalette) {
+    colorToBody.classList.remove(color);
+  }
+  colorToBody.classList.add(randomColor);
+}
+
+themeBtn.addEventListener("click", changeColor)
