e9cbbfc6055c3944997c819835e7f50232c87883
Jibon Krishna <jibonkrishnaroysetu@gmail.com>
2025-03-02 21:57:07 +0600

    added question-answers

---
+++ color.js
@@ -0,0 +1,14 @@
+const colorPalette = [
+  "bg-blue-100",
+  "bg-yellow-100",
+  "bg-purple-100",
+  "bg-orange-100",
+  "bg-teal-100",
+  "bg-green-100",
+  "bg-red-100",
+];
+// console.log(colorPalette);
+// for (color of colorPalette){
+//   console.log(color);
+// }
+
--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 document
 .getElementById("backToDesk-btn")
-.addEventListener("click", function (event) {
+.addEventListener("click", function () {
   window.location.href = "index.html";
 });
