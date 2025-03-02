d2cc3e680fe9282bf58a162ec08fcc487f5ab571
Jibon Krishna <jibonkrishnaroysetu@gmail.com>
2025-03-02 09:21:23 +0600

    Updated Contents

--- script.js
+++ script.js
@@ -16,11 +16,58 @@ document
     const convertedCount = parseInt(count);
     const sum = convertedCount - 1;
     document.getElementById("task-count").innerText = sum;
+
     const totalTaskCount = document.getElementById("total-task-count").innerText;
     const convertedTotalTaskCount = parseInt(totalTaskCount);
     const taskSum = convertedTotalTaskCount + 1;
     document.getElementById("total-task-count").innerText = taskSum;
+
+    const cardTitle = document.getElementById("card-title").innerText;
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+
+    const contaier = document.getElementById("history-container");
+    const div = document.getElementById("div");
+    div.classList.add("bg-green-100", "p-3", "m-3", "rounded-xl", "drop-shadow-md", "text-justify");
+    div.innerHTML = `
+    <p class="font-medium">
+    You have completed the task ${cardTitle} at ${time}.
+    </p>
+    `
+    contaier.appendChild(div);
   });
+
+
+
+
+// document
+//   .querySelectorAll(".board-btn")
+//   .addEventListener("click", function () {
+//     alert("Board Updated Successfully");
+//     const count = document.querySelector(".task-count").innerText;
+//     const convertedCount = parseInt(count);
+//     const sum = convertedCount - 1;
+//     document.getElementById("task-count").innerText = sum;
+
+//     const totalTaskCount = document.getElementById("total-task-count").innerText;
+//     const convertedTotalTaskCount = parseInt(totalTaskCount);
+//     const taskSum = convertedTotalTaskCount + 1;
+//     document.getElementById("total-task-count").innerText = taskSum;
+
+//     const cardTitle = document.getElementById("card-title").innerText;
+//     const now = new Date();
+//     const time = now.toLocaleTimeString();
+
+//     const contaier = document.getElementById("history-container");
+//     const div = document.getElementById("div");
+//     div.classList.add("bg-green-100", "p-3", "m-3", "rounded-xl", "drop-shadow-md", "text-justify");
+//     div.innerHTML = `
+//     <p class="font-medium">
+//     You have completed the task ${cardTitle} at ${time}.
+//     </p>
+//     `
+  //   contaier.appendChild(div);
+  // });
 
 // document
 //   .getElementById("total-task-count")
